60c9dcd1eefcc18aef7a37615705974b06858ccb
zephyr <zephyr.workmail@gmail.com>
2019-08-21 21:39:28 +0800

    add config

--- {{ projectName }}/__test__/index.test.ts
+++ {{ projectName }}/__test__/index.test.ts
@@ -1,9 +1,9 @@
-import { test } from 'aliyun-serverless-wrapper'
-import * as functions from '../src/index'
+import { test } from "aliyun-serverless-wrapper"
+import { {{ projectName }} } from "../src/index"
 
-it('should return json', async () => {
-  const { status, body, header } = await test(functions.{{ projectName }})()
+it("should return json", async () => {
+  const { status, body, header } = await test({{ projectName }})()
   expect(status).toBe(200)
-  expect(body).toBe('helloworld')
-  expect(header).toHaveProperty('Content-Type', 'text/plain')
+  expect(body).toBe("helloworld")
+  expect(header).toHaveProperty("Content-Type", "text/plain")
 })
--- {{ projectName }}/src/index.ts
+++ {{ projectName }}/src/index.ts
@@ -1,11 +1,11 @@
-import 'reflect-metadata'
-import { wrapper, AliyunHttpContext } from 'aliyun-serverless-wrapper'
+import "reflect-metadata"
+import { wrapper, AliyunHttpContext } from "aliyun-serverless-wrapper"
 
 export const initializer = (ctx: AliyunHttpContext, callback: any) => {
   // implement your own initializer here
-  callback(null, '')
+  callback(null, "")
 }
 
 export const {{ projectName }} = wrapper(async (ctx) => {
-  ctx.body = 'helloworld'
+  ctx.body = "helloworld"
 })
